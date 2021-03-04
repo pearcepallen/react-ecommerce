fb42ba921c2951b3c2b923a11a16a2342ecc1329
Pearce Allen <pearce.p.allen@gmail.com>
2021-03-04 16:50:16 -0500

    Added loader and message component

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,21 +3,21 @@ import { PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL } from '../constants/productConstants'
 
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try{
         dispatch({type: PRODUCT_LIST_REQUEST})
 
-        const { data } = await axios.get('/api/products/')
+        const { data } = await axios.get('/ap/products/')
 
         dispatch({
-            type:PRODUCT_LIST_SUCCESS,
+            type: PRODUCT_LIST_SUCCESS,
             payload: data
         })
 
     }catch(error){
         dispatch({
             type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.repsonse.data.message
+            payload: error.response && error.response.data.message
             ? error.response.data.message
             : error.message,
         })

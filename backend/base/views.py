950b262d64b72cb3ff8899901c02c3b0cf8f5bcd
Pearce Allen <pearce.p.allen@gmail.com>
2021-03-10 21:19:15 -0500

    Added User Serilization

--- backend/base/views.py
+++ backend/base/views.py
@@ -15,8 +15,10 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
@@ -34,11 +36,20 @@ def getRoutes(request):
             ]
     return Response(routes)
 
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getProduct(request, pk):

8d6cb776675ebdc2557cf93c56e38015c58e759a
Pearce Allen <pearce.p.allen@gmail.com>
2021-03-11 21:24:24 -0500

    Restructure views and url directory | backend

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import *
-from .serializers import *
-from .products import products
+from django.contrib.auth.models import User
+from base.models import *
+from base.serializers import *
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -60,19 +59,4 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-
     return Response(serializer.data)

380b41bf098bb72f699cd12645ad3c8e7f1e6893
Pearce Allen <pearce.p.allen@gmail.com>
2021-03-24 17:18:25 -0500

    Added admin user list reducer, action, screen

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -18,6 +18,11 @@ import {
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_RESET,
+
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
+    USER_LIST_RESET,
  } from '../constants/userConstants'
 
  export const userLoginReducer = (state={}, action) => {
@@ -86,13 +91,33 @@ export const userUpdateProfileReducer = (state={}, action) => {
             return {loading:true}
 
         case USER_UPDATE_PROFILE_SUCCESS:
-            return {loading:false, succes:true, userInfo: action.payload}
+            return {loading:false, success:true, userInfo: action.payload}
 
         case USER_UPDATE_PROFILE_FAIL:
             return {loading:false, error: action.payload}
 
         case USER_UPDATE_PROFILE_RESET:
             return {}
+
+        default:
+            return state
+    }
+}
+
+
+export const userListReducer = (state={users:[]}, action) => {
+    switch(action.type){
+        case USER_LIST_REQUEST:
+            return {loading:true}
+
+        case USER_LIST_SUCCESS:
+            return {loading:false, users: action.payload}
+
+        case USER_LIST_FAIL:
+            return {loading:false, error: action.payload}
+
+        case USER_LIST_RESET:
+            return {users: []}
 
         default:
             return state

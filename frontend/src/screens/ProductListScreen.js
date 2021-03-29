cbfb0b3a648cabcff97b15eae567b26d01465983
Pearce Allen <pearce.p.allen@gmail.com>
2021-03-29 17:09:53 -0500

    Added delete product endpoint and action, constants, reducer

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,13 +4,16 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { listProducts } from '../actions/productActions'
+import { listProducts, deleteProduct } from '../actions/productActions'
 
 function ProductListScreen({ history, match }) {
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList)
     const {loading, error, products} = productList
+
+    const productDelete = useSelector(state => state.productDelete)
+    const {loading: loadingDelete, error: errorDelete, success: successDelete} = productDelete
 
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
@@ -22,12 +25,12 @@ function ProductListScreen({ history, match }) {
         else{
             history.push('/login')
         }
-    }, [dispatch, history, userInfo])
+    }, [dispatch, history, userInfo, successDelete])
 
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure you want to delete this product?')){
-            //Delete product
+            dispatch(deleteProduct(id))
         }
         
     }
@@ -48,6 +51,9 @@ function ProductListScreen({ history, match }) {
                     </Button>
                 </Col>
             </Row>
+
+            {loadingDelete && <Loader />}
+            {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
 
             {loading 
             ? (<Loader />) 

af9e3d4d3eff96497b3db3ad42c9cd2f06901be7
Pearce Allen <pearce.p.allen@gmail.com>
2021-04-05 16:16:19 -0500

    Added Product Search

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,16 +7,16 @@ import Message from '../components/Message'
 import { listProducts } from '../actions/productActions'
 
 
-function HomeScreen() {
+function HomeScreen({history}) {
     const dispatch = useDispatch()
     const productList = useSelector(state => state.productList)
     const {error, loading, products} = productList
 
+    let keyword = history.location.search
+    console.log(keyword)
     useEffect(() => {
-        dispatch(listProducts())
-        
-
-    }, [dispatch])
+        dispatch(listProducts(keyword))
+    }, [dispatch, keyword])
 
     return (
         <div>
